069a0919dd6b120fe855538578046f331a6a8ca8
Jeff Ripke <jripke74@gmail.com>
2021-08-03 10:31:04 -0700

    add delete

--- app.js
+++ app.js
@@ -50,7 +50,22 @@ const storage = new GridFsStorage({
 const upload = multer({ storage });
 
 app.get('/', (req, res) => {
-  res.render('index');
+  gfs.files.find().toArray((err, files) => {
+    if (!files || files.length === 0) {
+      res.render('index', { files: false });
+    } else {
+      files.map(file => {
+        if (file.contentType === 'image/jpeg'
+          || file.contentType === 'image/jpg'
+          || file.contentType === 'image/png') {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.render('index', { files: files });
+    }
+  });
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
@@ -97,6 +112,17 @@ app.get('/image/:filename', (req, res) => {
         err: 'Not an image',
       });
     }
+  });
+});
+
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err) => {
+    if (err) {
+      return res.status(404).json({
+        err: err,
+      });
+    }
+    res.redirect('/');
   });
 });
 

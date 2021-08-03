5300573652da972e3117afae69666249a717caa9
Jeff Ripke <jripke74@gmail.com>
2021-08-03 06:23:14 -0700

    add stream of files

--- app.js
+++ app.js
@@ -1,20 +1,72 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
+const { GridFsStorage } = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const methodOveride = require('method-override');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(methodOveride('_method'));
 app.set('view engine', 'ejs');
+
+const mongoURI =   // eslint-disable-next-line max-len
+'mongodb+srv://user1:nWyWzsOk4ks7Kd9F@cluster0.yhsnj.mongodb.net/shop?retryWrites=true&w=majority';
+
+const conn = mongoose.createConnection(mongoURI,
+  { useNewUrlParser: true });
+
+let gfs;
+
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads',
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist',
+      });
+    }
+
+    return res.json(files);
+  });
 });
 
 const port = 5000;
